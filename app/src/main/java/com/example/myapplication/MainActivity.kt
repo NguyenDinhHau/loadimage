790d0ac93fd5852c7f7b725fe9354f246b23f52e
Nguyen Dinh Hau <nguyendinhhau.98.hnd@gmail.com>
2019-08-22 05:04:13 +0700

    da thay doi moi

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,11 +2,20 @@ package com.example.myapplication
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        btn_load.setOnClickListener {
+            Picasso.with(this)
+                    .load("https://img2.thuthuatphanmem.vn/uploads/2018/12/25/nhung-hinh-anh-gai-xinh-cuc-dep_012909400.jpg")
+                    .error(R.drawable.anh1)
+                    .placeholder(R.drawable.vu_3)
+                    .into(img_view)
+        }
     }
 }
