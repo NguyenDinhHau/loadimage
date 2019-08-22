52861fb63b5ef8fdefa78086ef3a937f7e99cc83
Nguyen Dinh Hau <nguyendinhhau.98.hnd@gmail.com>
2019-08-22 12:55:45 +0700

    da them glide

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.myapplication
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -15,6 +16,11 @@ class MainActivity : AppCompatActivity() {
                     .load("https://img2.thuthuatphanmem.vn/uploads/2018/12/25/nhung-hinh-anh-gai-xinh-cuc-dep_012909400.jpg")
                     .error(R.drawable.anh1)
                     .placeholder(R.drawable.vu_3)
+                    .into(img_view)
+        }
+        btn_glide.setOnClickListener {
+            Glide.with(this)
+                    .load("https://www.baodatviet.com/wp-content/uploads/2018/09/anh-gai-xinh-trong-bo-ao-ngu-sexy-nghet-tho-ab3368.jpg")
                     .into(img_view)
         }
     }
